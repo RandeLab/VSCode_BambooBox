868430bce69fbf3fd5cc4f35481bba9400b4ada9
Marc Dusseiller <marc@dusseiller.ch>
2018-06-30 13:12:01 +0200

    Update Our-Sci_MH-Z16_BambooBox_v3_longMeas.ino

--- src/Our-Sci_MH-Z16_BambooBox_v3_longMeas.ino
+++ src/Our-Sci_MH-Z16_BambooBox_v3_longMeas.ino
@@ -11,7 +11,7 @@
 #define HIGH_RES_INTERVAL 2
 #define LOW_RES_INTERVAL_FACT 60
 
-#define HIGH_RES_COUNT 900 // was 150
+#define HIGH_RES_COUNT 1000 // was 150
 #define LOW_RES_COUNT 150 // was 150
 
 #define PIN_STARTBUTTON 8
